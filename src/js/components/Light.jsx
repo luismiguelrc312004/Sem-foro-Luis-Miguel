c951f92423f045178e34d2d7e967c74e310d4d4e
Luis Miguel RC312004 <luismiguelrc312004@gmail.com>
2026-02-17 15:53:36 +0000

    Added Cambios del codigo

--- src/js/components/Light.jsx
+++ src/js/components/Light.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import "./Light.css"
 
-function Light({color}) {
+function Light({ color }) {
   return (
-      <div className='spotlight' style={{backgroundColor: color}}>
-          </div>
-            )
-            }
+    <div 
+      className='spotlight' 
+      style={{ backgroundColor: color, color: color }}>
+    </div>
+  )
+}
 
-            export default Light;
-            
+export default Light;
--- src/js/components/TrafficLight.jsx
+++ src/js/components/TrafficLight.jsx
@@ -2,62 +2,46 @@ import React, { useState } from 'react'
 import Light from './Light'
 import "./TrafficLight.css"
 
-
-
 function TrafficLight() {
   const [selectedColor, setSelectedColor] = useState("red");
   const [showPurple, setShowPurple] = useState(false);
 
-  const togglePurple = () => {
-    setShowPurple(prev => !prev);
-  };
-
-  const colors = showPurple
-    ? ["red", "goldenRod", "green", "purple"]
-    : ["red", "goldenRod", "green"];
+  const colorsLight = ["red", "goldenRod", "green"];
+  const colors = showPurple ? [...colorsLight, "purple"] : colorsLight;
 
   const changeColor = () => {
-    const currentColor = colors.indexOf(selectedColor)
-    const nextColor = (currentColor + 1) % colors.length
-    setSelectedColor(colors[nextColor])
-  }
+    const nextIndex = (colors.indexOf(selectedColor) + 1) % colors.length;
+    setSelectedColor(colors[nextIndex]);
+  };
+
+  const togglePurple = () => setShowPurple(!showPurple);
 
   return (
-    <>
-      <div className='container'>
-        <div className='handle'>
-        </div>
-        <div className='bodyTrafficLight'>
-          <div onClick={() => setSelectedColor("red")} className={(selectedColor === "red" ? "glowRed" : "")}>
-            <Light color='red' />
+    <div className='container'>
+      <div className='handle'>
+      </div>
+      <div className='bodyTrafficLight'>
+        {colors.map(color => (
+          <div
+            key={color}
+            onClick={() => setSelectedColor(color)}
+            className={`${selectedColor === color ? "active" : ""}`}
+            style={{ "--glow-color": color }}
+          >
+            <Light color={color} />
           </div>
-          <div onClick={() => setSelectedColor("goldenRod")} className={(selectedColor === "goldenRod" ? "glowGoldenRod" : "")}>
-            <Light color='goldenRod' />
-          </div>
-          <div onClick={() => setSelectedColor("green")} className={(selectedColor === "green" ? "glowGreen" : "")}>
-            <Light color='green' />
-          </div>
-          {showPurple && (
-            <div
-              onClick={() => setSelectedColor("purple")}
-              className={selectedColor === "purple" ? "glowPurple" : ""}
-            >
-              <Light color="purple" />
-            </div>
-          )}
-
-        </div>
-        <div className='row mt-3'>
-          <button className='btnChange' onClick={changeColor}>
-            Cambiar Color
-          </button>
-          <button className='btnChange' onClick={togglePurple}>
-            Mostrar / Ocultar Púrpura
-          </button>
-        </div>
+        ))}
 
       </div>
-    </>
+      <div className='row mt-3'>
+        <button onClick={changeColor}>
+          Cambiar Color
+        </button>
+        <button onClick={togglePurple}>
+          Mostrar / Ocultar Púrpura
+        </button>
+      </div>
+    </div>
   )
 }
 
